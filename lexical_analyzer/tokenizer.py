74baa68080c7804d0ad6208e0c5cc296162be9c6
SMS-Sajadi <Soroush.s2017@gmail.com>
2024-04-18 12:27:38 +0330

    fixing the problem of tokens that are keywords and also recognized as an id.

--- lexical_analyzer/tokenizer.py
+++ lexical_analyzer/tokenizer.py
@@ -15,6 +15,9 @@ def tokenize(program: str) -> List[Token]:
     lexeme_begin: int = 0
     line: int = 1
     while lexeme_begin < len(program):
+        max_forward: int = 1  # TODO: set it to 0
+        final_token: Token | None = None
+
         is_new_line, is_tab, whitespace_token = ld.is_whitespace(program[lexeme_begin:])
         if whitespace_token is not None:
             if is_tab:
@@ -27,48 +30,43 @@ def tokenize(program: str) -> List[Token]:
             continue
 
         forward, token = ld.is_bool(program[lexeme_begin:])
-        if token is not None:
-            tokens.append(token)
-            lexeme_begin += forward
-            continue
+        if token is not None and forward > max_forward:
+            max_forward = forward
+            final_token = token
 
         forward, token = ld.is_break(program[lexeme_begin:])
-        if token is not None:
-            tokens.append(token)
-            lexeme_begin += forward
-            continue
+        if token is not None and forward > max_forward:
+            max_forward = forward
+            final_token = token
 
         forward, token = ld.is_char(program[lexeme_begin:])
-        if token is not None:
-            tokens.append(token)
-            lexeme_begin += forward
-            continue
+        if token is not None and forward > max_forward:
+            max_forward = forward
+            final_token = token
 
         forward, token = ld.is_comment(program[lexeme_begin:])
-        if token is not None:
-            tokens.append(token)
-            lexeme_begin += forward
-            continue
+        if token is not None and forward > max_forward:
+            max_forward = forward
+            final_token = token
+            line += 1
 
         forward, token = ld.is_string(program[lexeme_begin:])
-        if token is not None:
-            tokens.append(token)
-            lexeme_begin += forward
-            continue
+        if token is not None and forward > max_forward:
+            max_forward = forward
+            final_token = token
 
         forward, token = ld.is_character(program[lexeme_begin:])
-        if token is not None:
-            tokens.append(token)
-            lexeme_begin += forward
-            continue
+        if token is not None and forward > max_forward:
+            max_forward = forward
+            final_token = token
 
         forward, token = ld.is_id(program[lexeme_begin:])
-        if token is not None:
-            tokens.append(token)
-            lexeme_begin += forward
-            continue
+        if token is not None and forward > max_forward:
+            max_forward = forward
+            final_token = token
 
-        lexeme_begin += 1
+        lexeme_begin += max_forward
+        tokens.append(final_token)
 
         # detected_tokens: List[Tuple[int, Token | None]] = list()
 
