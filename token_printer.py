60f6c539995588a5d147b36b56df21c4e8493590
SMS-Sajadi <Soroush.s2017@gmail.com>
2024-04-20 13:18:55 +0330

    adding token printer function for seeing results

---
+++ token_printer.py
@@ -0,0 +1,23 @@
+"""
+    This file is used for pretty printing tokens.
+    This implementation is used for PL Language Compiler (PL) lexical analyzer, and
+    should not be used directly.
+    (c) 2024|1403
+"""
+from typing import List
+from lexical_analyzer.token_base import Token
+
+
+def print_tokens(tokens: List[Token], *, with_ws_print: bool =True) -> None:
+    """
+    This Function will print the tokens in lexical analyzer.
+    :param tokens:
+    :param with_ws_print:
+    :return:
+    """
+    if with_ws_print:
+        print(*tokens, sep='\n')
+    else:
+        for token in tokens:
+            if token.type != "T_Whitespace":
+                print(token)
