13ae215f1cd2d6c812af4c16e95b43236dedf0cc
SMS-Sajadi <Soroush.s2017@gmail.com>
2024-04-20 13:40:24 +0330

    adding doc string

--- token_printer.py
+++ token_printer.py
@@ -11,8 +11,8 @@ from lexical_analyzer.token_base import Token
 def print_tokens(tokens: List[Token], *, with_ws_print: bool =True) -> None:
     """
     This Function will print the tokens in lexical analyzer.
-    :param tokens:
-    :param with_ws_print:
+    :param tokens: List of tokens.
+    :param with_ws_print: If it is true, the withspaces will be printed.
     :return:
     """
     if with_ws_print:
