c465f833dd6ed5f08c886b72af2ead243262b17d
SMS-Sajadi <Soroush.s2017@gmail.com>
2024-06-01 11:17:51 +0330

    adding the function call support, it has some bugs and problems that should be fixed

--- syntax_analyzer/parser_table.py
+++ syntax_analyzer/parser_table.py
@@ -5,7 +5,8 @@
     (c) 2024|1403
 """
 
-VARS = ['Program', 'functions', 'Type', 'function_params', 'Stmts', 'param', 'params_list', 'const_bracket']
+VARS = ['Program', 'functions', 'Type', 'function_params', 'Stmts', 'param', 'params_list', 'const_bracket',
+        'Stmts', 'stmt', 'Declaration', 'Declarations', ]
 
 M = {
     'Program': {
@@ -63,6 +64,7 @@ M = {
         'T_Break': ['stmt', 'Stmts'],
         'T_Continue': ['stmt', 'Stmts'],
         'T_Return': ['stmt', 'Stmts'],
+        'T_Id': ['stmt', 'Stmts'],
         # Follow
         'T_RC': ['epsilon'],
     },
@@ -115,16 +117,36 @@ M = {
         'T_Break': ['other_stmt'],
         'T_Continue': ['other_stmt'],
         'T_Return': ['other_stmt'],
+        'T_Id': ['function_call', 'T_Semicolon']
     },
     'other_stmt': {
         # First
         'T_Break': ['T_Break', 'T_Semicolon'],
         'T_Continue': ['T_Continue', 'T_Semicolon'],
         'T_Return': ['T_Return', 'Exp', 'T_Semicolon'],
+    },
+    'function_call': {
+        # First
+        'T_Id': ['T_Id', 'T_LP', 'Args', 'T_RP'],
+    },
+    'Args': {
+        # First
+        'T_Id': ['Exp', 'Args_list'],
+        # TODO: Continue first of Expr
+        # Follow
+        'T_RP': ['epsilon'],
+    },
+    'Args_list': {
+        # First
+        'T_Comma': ['T_Comma', 'Exp', 'Args_list'],
+        # Follow
+        'T_RP': ['epsilon'],
     }
     # TODO: Continue to Infinity!
     # TODO: Write const_num
     # TODO: Complete the Stmts ans stmt
+    # TODO: Write Exp
+    # TODO: Check Semicolons
 }
 
 
