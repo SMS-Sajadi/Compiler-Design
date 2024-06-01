713dff9504c1f6c2be9d7fc5e2a7dc3aa74a290a
SMS-Sajadi <Soroush.s2017@gmail.com>
2024-06-01 10:56:55 +0330

    adding more details about first and follow of rules

--- syntax_analyzer/parser_table.py
+++ syntax_analyzer/parser_table.py
@@ -9,38 +9,49 @@ VARS = ['Program', 'functions', 'Type', 'function_params', 'Stmts', 'param', 'pa
 
 M = {
     'Program': {
+        # First
         'T_Int': ['function', 'Program'],
         'T_Char': ['function', 'Program'],
         'T_Bool': ['function', 'Program'],
+        # Follow
         '$': ['epsilon'],
     },
     'function': {
+        # First
         'T_Int': ['Type', 'T_Id', 'T_LP', 'function_params', 'T_RP', 'T_LC', 'Stmts', 'T_RC'],
         'T_Char': ['Type', 'T_Id', 'T_LP', 'function_params', 'T_RP', 'T_LC', 'Stmts', 'T_RC'],
         'T_Bool': ['Type', 'T_Id', 'T_LP', 'function_params', 'T_RP', 'T_LC', 'Stmts', 'T_RC'],
     },
     'Type': {
+        # First
         'T_Int': ['T_Int'],
         'T_Char': ['T_Char'],
         'T_Bool': ['T_Bool'],
     },
     'function_params': {
+        # First
         'T_Int': ['param', 'params_list'],
         'T_Char': ['param', 'params_list'],
         'T_Bool': ['param', 'params_list'],
+        # Follow
         'T_RP': ['epsilon'],
     },
     'params_list': {
+        # First
         'T_Comma': ['T_Comma', 'param', 'params_list'],
+        # Follow
         'T_RP': ['epsilon'],
     },
     'param': {
+        # First
         'T_Int': ['Type', 'T_Id', 'const_bracket'],
         'T_Char': ['Type', 'T_Id', 'const_bracket'],
         'T_Bool': ['Type', 'T_Id', 'const_bracket'],
     },
     'const_bracket': {
+        # First
         'T_LB': ['T_LB', 'const_num', 'T_RB', 'const_bracket'],
+        # Follow
         'T_Comma': ['epsilon'],
         'T_RP': ['epsilon'],
     },
@@ -53,42 +64,55 @@ M = {
         'T_RC': ['epsilon'],
     },
     'Declaration': {
+        # First
         'T_Int': ['Type', 'Declarations', 'T_Semicolon'],
         'T_Char': ['Type', 'Declarations', 'T_Semicolon'],
         'T_Bool': ['Type', 'Declarations', 'T_Semicolon'],
     },
     'Declarations': {
+        # First
         'T_Id': ['var_declaration', 'Declaration_list'],
+        # Follow
         'T_Semicolon': ['epsilon'],
     },
     'Declaration_list': {
+        # First
         'T_Comma': ['T_Comma', 'var_declaration', 'Declaration_list'],
+        # Follow
         'T_Semicolon': ['epsilon'],
     },
     'var_declaration': {
+        # First
         'T_Id': ['mutable', 'Assign'],
     },
     'mutable': {
+        # First
         'T_Id': ['T_Id', 'bracket'],
     },
     'bracket': {
+        # First
         'T_LB': ['T_LB', 'Exp', 'T_RB', 'bracket'],
+        # Follow
         'T_Assign': ['epsilon'],
         'T_Comma': ['epsilon'],
         'T_Semicolon': ['epsilon'],
     },
     'Assign': {
+        # First
         'T_Assign': ['T_Assign', 'Exp'],
+        # Follow
         'T_Comma': ['epsilon'],
         'T_Semicolon': ['epsilon'],
     },
     'stmt': {
+        # First
         'T_Int': ['Declaration'],
         'T_Char': ['Declaration'],
         'T_Bool': ['Declaration'],
     }
     # TODO: Continue to Infinity!
     # TODO: Write const_num
+    # TODO: Complete the Stmts ans stmt
 }
 
 
