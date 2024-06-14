28a4801cb4b80c13916e745f4ddbd6bd4ae360fe
SMS-Sajadi <Soroush.s2017@gmail.com>
2024-06-14 12:10:38 +0330

    cleaning the parser code

--- syntax_analyzer/parser.py
+++ syntax_analyzer/parser.py
@@ -8,7 +8,11 @@ from typing import List
 from syntax_analyzer.parser_table import get_table
 from lexical_analyzer.token_base import Token
 from syntax_analyzer.special_node import Node
-from syntax_analyzer.error_handlers import raise_error_empty_table, raise_error_synch, raise_error_suggest, raise_error_end
+from syntax_analyzer.AutoParserTableCreator.grammar_loader import get_auto_table
+from syntax_analyzer.error_handlers import (raise_error_empty_table, raise_error_synch, raise_error_suggest,
+                                            raise_error_end)
+
+from deepdiff import DeepDiff
 
 
 def parse(tokens: List[Token]) -> Node:
@@ -21,22 +25,17 @@ def parse(tokens: List[Token]) -> Node:
     tokens.append(Token('$', token_line=tokens[-1].line + 1, token_inline_index=0))
 
     token_idx: int = 0
-    table = get_table()
+    # table = get_table()
+    table = get_auto_table()
 
     tree_stack = ['$', tree]
     top_of_stack: str = stack.pop()
     top_of_tree = tree_stack.pop()
     is_error_seen: bool = False
-    is_synch_error: bool = False
 
     exceptions: List[Exception] = []
 
     while top_of_stack != '$':
-        # if top_of_stack == 'synch':
-        #     top_of_stack = stack.pop()
-        #     exceptions.append(
-        #         raise_error_synch(tokens, token_idx, top_of_stack)
-        #     )
 
         if top_of_stack[:2] == 'T_':
             if tokens[token_idx].type == top_of_stack:
@@ -44,7 +43,6 @@ def parse(tokens: List[Token]) -> Node:
                 token_idx += 1
             else:
                 is_error_seen = True
-                is_synch_error = False
                 exceptions.append(
                     raise_error_suggest(tokens[token_idx - 1], top_of_stack)
                 )
@@ -53,11 +51,9 @@ def parse(tokens: List[Token]) -> Node:
                 body = table[top_of_stack][tokens[token_idx].type].copy()
                 if 'synch' in body:
                     is_error_seen = True
-                    if not is_synch_error:
-                        exceptions.append(
-                            raise_error_synch(tokens, token_idx, top_of_stack)
-                        )
-                    is_synch_error = True
+                    exceptions.append(
+                        raise_error_synch(tokens, token_idx, top_of_stack)
+                    )
                     top_of_stack = stack.pop()
                     continue
 
@@ -75,7 +71,6 @@ def parse(tokens: List[Token]) -> Node:
                 tree_stack.extend(reversed(temp))
             except KeyError as e:
                 is_error_seen = True
-                is_synch_error = False
                 if token_idx + 1 == len(tokens):
                     exceptions.append(
                         raise_error_end(tokens, token_idx, top_of_stack)
