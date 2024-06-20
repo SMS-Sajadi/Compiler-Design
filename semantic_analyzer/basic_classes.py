e7d70c27776ca70aeef3040ef8aae4955d8fa359
SMS-Sajadi <Soroush.s2017@gmail.com>
2024-06-20 22:15:03 +0330

    adding basic classes that we need for function table and vars table

---
+++ semantic_analyzer/basic_classes.py
@@ -0,0 +1,14 @@
+"""
+    Here we have the basic function table and variable declarations table.
+    This implementation is used for PL Language Compiler (PL) semantic analyzer, and
+    should not be used directly.
+    (c) 2024|1403
+"""
+
+
+class Function:
+    def __init__(self, name):
+        self.name = name
+        self.entries = []
+        self.return_type = None
+        self.has_return = False
